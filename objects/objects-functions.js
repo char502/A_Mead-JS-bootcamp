50f17c0f002c031ee66a84d0296f7da06eb9309d
char502 <char502@hotmail.com>
2019-01-16 11:48:22 +0000

    objects review

--- objects/objects-functions.js
+++ objects/objects-functions.js
@@ -9,3 +9,23 @@ let otherBook = {
   author: "Howard Zinn",
   pageCount: 723
 };
+
+let getSummary = function(book) {
+  console.log(`${book.title} by ${book.author}`);
+  //   (this.title = title), (this.author = author), (this.pageCount = pageCount);
+};
+
+getSummary(myBook);
+getSummary(otherBook);
+
+// const getSummary = new Obj("Joe", 5, "blue");
+// console.log(createObj);
+
+// const Obj = function(title, author, pageCount) {
+//   this.title = title,
+//   this.author = author,
+//   this.pageCount = pageCount;
+// };
+
+// const createObj = new Obj("Joe", 5, "blue");
+// console.log(createObj);
