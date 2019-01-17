235ba5b736feb145e82846a7685021eda691523c
char502 <char502@hotmail.com>
2019-01-17 17:34:44 +0000

    object review

--- objects/objects-functions.js
+++ objects/objects-functions.js
@@ -10,13 +10,74 @@ let otherBook = {
   pageCount: 723
 };
 
-let getSummary = function(book) {
-  console.log(`${book.title} by ${book.author}`);
-  //   (this.title = title), (this.author = author), (this.pageCount = pageCount);
+// passing an object into a function
+
+// let getSummary = function(book) {
+//   console.log(`${book.title} by ${book.author}`);
+//   (this.title = title), (this.author = author), (this.pageCount = pageCount);
+// };
+
+// getSummary(myBook);
+// getSummary(otherBook);
+
+// returning an object from a function
+
+//==================================================================
+
+// let getSummary = function(book) {
+//   return {
+//     summary: `${book.title} by ${book.author}`,
+//     pageCountSummary: `${book.title} is ${book.pageCount} pages long`
+//   };
+// };
+// let bookSummary = getSummary(myBook);
+// let otherBookSummary = getSummary(otherBook);
+
+// console.log(bookSummary.pageCountSummary);
+// console.log(otherBookSummary.summary);
+
+// ===================================================================
+
+// create a function that returns an object
+
+// return an object that contains all 3 temps
+// pass in fahrenheit and convert to both Celsius and Kelvin
+
+let convTemps = function(fahrenheit) {
+  return {
+    fahrenheit: `${fahrenheit}`,
+    Kelvin: `${(fahrenheit + 459.67) / 1.8}`,
+    Celsius: `${(fahrenheit - 32) / 1.8}`
+  };
 };
 
-getSummary(myBook);
-getSummary(otherBook);
+let temp = convTemps(74);
+// let tempC = convTemps(64);
+// let tempK = convTemps(120);
+console.log(temp);
+// console.log(tempC);
+// console.log(tempK);
+
+// let convertFahrenheitToCelsius = function (fah) {
+//     //   console.log("welcome user");
+//     let convert = (fah - 32) / 1.8;
+//     return convert;
+// };
+
+// let conv = convertFahrenheitToCelsius(32);
+// let convOther = convertFahrenheitToCelsius(68);
+// console.log(conv);
+// console.log(convOther);
+
+// let celsius = (fahrenheit - 32) / 1.8;
+// let kelvin = (fahrenheit + 459.67) / 1.8;
+
+// =====================================================================
+
+// console.log(`${book.title} by ${book.author}`);
+
+//   (this.title = title), (this.author = author), (this.pageCount = pageCount);
+// };
 
 // const getSummary = new Obj("Joe", 5, "blue");
 // console.log(createObj);
---
+++ objects/objects-references.js
@@ -0,0 +1,66 @@
+// let myAccount = {
+//   name: "Char",
+//   expenses: 0,
+//   income: 0
+// };
+
+// let addExpense = function(account, amount) {
+//   account.expenses = account.expenses + amount;
+// };
+
+// functions to create:
+
+// addIncome - takes the amount and income
+
+// resetAccount - reset the expenses and the income for an a/c to 0 (rq's which account to change)
+
+// getAccountSummary - includes current account balance, totals in expense income
+// (can return the string as the return value for 'getAccountSummary', when want to use it, use console.log)
+
+// Account for (Account) has $900. 1000 in income. $100 in expenses
+
+// addExpense(myAccount, 2.5);
+// console.log(myAccount);
+
+let myAccount = {
+  name: "Char",
+  expenses: 0,
+  income: 0
+};
+
+let addExpense = function(account, amount) {
+  account.expenses = account.expenses + amount;
+};
+
+let addIncome = function(account, income) {
+  account.income = account.income + income;
+  // console.log(account);
+};
+
+let resetAccount = function(account) {
+  // account.expenses = account.expenses = 0;
+  account.income = 0;
+  // console.log(account);
+};
+
+let getAccountSummary = function(account) {
+  let balance = account.income - account.expenses;
+  return `Account for ${account.name} has £${balance}. £${
+    account.income
+  } in income. £${account.expenses} in expenses`;
+};
+
+// let addIncomeAmount = addIncome(myAccount, 50, 250);
+addIncome(myAccount, 250);
+console.log(myAccount);
+addExpense(myAccount, 50);
+console.log(myAccount);
+addExpense(myAccount, 100);
+console.log(myAccount);
+// resetAccount(myAccount);
+// console.log(myAccount);
+let c = getAccountSummary(myAccount);
+console.log(c);
+
+// let resetAccountVals = resetAccount();
+// let seeAccountSummary = getAccountSummary();
