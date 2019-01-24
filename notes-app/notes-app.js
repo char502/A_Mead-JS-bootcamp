ca862d93c3bb659e8defd94ea83c64fd2461a062
char502 <char502@hotmail.com>
2019-01-24 10:37:14 +0000

    DOM Manipulation practice

---
+++ notes-app/notes-app.js
@@ -0,0 +1,107 @@
+const notes = [
+  {
+    title: "my next trip",
+    body: "I would like to go to Spain"
+  },
+  {
+    title: "Habits to work on",
+    body: "Exercise. Eating a bit better"
+  },
+  {
+    title: "Office Modification",
+    body: "Get a new Seat"
+  }
+];
+
+document.querySelector("#create-note").addEventListener("click", function(e) {
+  console.log("Create Note Clicked");
+  console.log(e);
+  e.target.textContent = "The button was clicked";
+});
+
+document.querySelector("#remove-all").addEventListener("click", function() {
+  console.log("Remove All Notes Clicked");
+  document.querySelectorAll(".note").forEach(function(note) {
+    note.remove();
+  });
+});
+
+document.querySelector("#search-text").addEventListener("input", function(e) {
+  console.log(e.target.value);
+});
+
+// using input event on event listener will fire on every single character change
+// allows filtering in real time
+
+// =========================================================================
+
+// document.querySelector("#search-text").addEventListener("change", function(e) {
+//   console.log(e.target.value);
+// });
+//
+// using 'change' in event listener, only works when click away
+
+// =========================================================================
+
+// e.target.value
+// use to access the data that the user typed in
+// can also use to override the data from javaScript
+// similar concept to textContent with the other eleemnts
+//========
+// can read a h1's text value using 'textContent', can also set it
+//========
+// the same is true of our inputs using value
+
+// =========================================================================
+
+// Other selector examples:
+
+// ----Single----
+// p
+// #replace
+// .item
+
+// ----Multiple----
+// p#order (paragraph tag with id of order)
+// button.inventory (all buttons with a class name of inventory)
+// h1#title.application (target a h1 that has an id of 'title' and a class of 'application')
+// h1.application#title(target a h1 that has a class of 'application' and an id of 'title')
+
+// order the two come in don't matter, only important thing is that your tag ('p', 'button', 'h1' etc )comes first (when setting up multiple things in your selector)
+
+// =========================================================================
+
+// querySelector - typically used with id's because just one
+// querySelectorAll - typically used with classes because trying to get EVERYTHING that has that class
+// =========================================================================
+// Event Listener = a function to run when an event happens
+// target - repesentation of DOM element
+// =========================================================================
+// can get the element that the event was fired on - e.target (event.target)
+// e.target (gives you the element)
+
+// e event arg is useful when want to do something with the element that the event actually fired on
+
+// =============================================================
+// =============================================================
+// DOM - Document object model
+
+// querySelector - only matches the first match it finds
+
+// Query and remove
+// const p = document.querySelector("p");
+// p.remove();
+
+// Query all and remove
+// const ps = document.querySelectorAll("p");
+// // console.log(p);
+// ps.forEach(function(item) {
+//   item.textContent = "********";
+//   //   console.log(item.textContent);
+//   //   item.remove();
+// });
+
+// Add a new element
+// const newParagraph = document.createElement("p");
+// newParagraph.textContent = "This is a new element from JS";
+// document.querySelector("body").appendChild(newParagraph);
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,9 +1,76 @@
-const ps = document.querySelectorAll("p");
-
-ps.forEach(function(item) {
-  //   console.log(item.textContent.includes("the"));
-
-  if (item.textContent.includes("the".toLowerCase())) {
-    item.remove();
+let todos = [
+  {
+    text: "swim 3 lengths",
+    completed: true
+  },
+  {
+    text: "Eat dinner",
+    completed: false
+  },
+  {
+    text: "Go Fishing",
+    completed: true
+  },
+  {
+    text: "Go Running",
+    completed: false
+  },
+  {
+    text: "Get some sleep",
+    completed: true
   }
-});
+];
+
+// lsiten for new todo creation
+const btn = document
+  .querySelector("#add-todo")
+  .addEventListener("click", function(e) {
+    console.log("adding a new todo");
+    e.target.textContent = "button clicked";
+  });
+
+// Add a new button with some text
+// add a listener to that button
+// print a message to the console ('adding a new todo')
+
+// =========================================================
+// =========================================================
+
+// How many todos still need to complete
+// You have have x todos left (p element)
+
+const toCompleteMessage = function(todos) {
+  let toComplete = todos.filter(function(todo) {
+    return !todo.completed;
+  });
+
+  const completedMessage = document.createElement("h2");
+  completedMessage.textContent = `You have ${
+    toComplete.length
+  } todos left to complete`;
+  document.querySelector("body").appendChild(completedMessage);
+  // console.log(toComplete.length);
+  // return toComplete.length;
+};
+
+toCompleteMessage(todos);
+// add a paragraph for each todo above (use text value)
+const updateTodos = function(todos) {
+  todos.forEach(function(todo) {
+    const todoItem = document.createElement("p");
+    todoItem.textContent = todo.text;
+    document.querySelector("body").appendChild(todoItem);
+  });
+};
+
+updateTodos(todos);
+
+// const ps = document.querySelectorAll("p");
+
+// ps.forEach(function(item) {
+//   //   console.log(item.textContent.includes("the"));
+
+//   if (item.textContent.includes("the".toLowerCase())) {
+//     item.remove();
+//   }
+// });
