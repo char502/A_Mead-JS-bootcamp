0893c4720b27f60fa9673ea9373d5856ed842d0e
char502 <char502@hotmail.com>
2019-02-05 21:13:29 +0000

    JS OOP: prototypal inheritance review

--- OOP/hangman.js
+++ OOP/hangman.js
@@ -1,13 +1,37 @@
 // prototypal ingeritance
 
 // Not arrow function for constructor - arrow functions do not bind 'this'
-const Hangman = function(word, remainingGuesses) {
-  this.word = word;
+const Hangman = function(word, guessedLetters = [], remainingGuesses) {
+  this.word = word.toLowerCase().split("");
+  this.guessedLetters = guessedLetters;
   this.remainingGuesses = remainingGuesses;
 };
 
-const game1 = new Hangman("cat", 2);
-const game2 = new Hangman("magazine", 5);
+// this prototype gets set to the Hangman prototype in the constructor (behind the scenes) which then makes the prototype available to all object instances created by the constructor
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = "";
+  // let newArr = [];
 
-console.log(game1);
-console.log(game2);
+  this.word.forEach((letter) => {
+    if (this.guessedLetters.indexOf(letter) !== -1 || letter === " ") {
+      return (puzzle += letter);
+    } else {
+      return (puzzle += "*");
+    }
+  });
+
+  console.log(Hangman.prototype);
+  // console.log(puzzle);
+  // puzzle = newArr.join("");
+  return puzzle;
+};
+
+const game1 = new Hangman("Baby Shark gone home", ["b", "k", "h", "o"], 3);
+// const game1 = new Hangman("Gone Home", ["e", "t"], 5);
+const game2 = new Hangman("magazine", 0, 8);
+
+// console.log(game1);
+// console.log(game2);
+
+console.log(game1.getPuzzle());
+// console.log(game2.getPuzzle());
--- OOP/person.js
+++ OOP/person.js
@@ -1,4 +1,4 @@
-const Person = function(firstName, lastName, age) {
+const Person = function(firstName, lastName, age, likes = []) {
   //   console.log(this);
   this.firstName = firstName;
   this.lastName = lastName;
@@ -10,10 +10,25 @@ const Person = function(firstName, lastName, age) {
 // Prototypal inheritance
 
 Person.prototype.getBio = function() {
-  return `${this.firstName} is ${this.age}.`;
+  let bio = `${this.firstName} is ${this.age}.`;
+
+  this.likes.forEach((like) => {
+    bio += ` ${this.firstName} likes ${like} `;
+    // Can use 'this' inside the function
+    // Arrow funtions do not bind a 'this' value
+    // Arrow functions inherit the 'this' value from their parent
+    // the parent has the correct 'this' value for the instance
+    // An ES5 function would not work here
+  });
+
+  return bio;
 };
 
+// ========================================================================
+// prototype object is linked to the constructor - any changes made to the prototype object will be reflected in all of the constructor instances, even if the change is made after the instance is created
+// ========================================================================
 Person.prototype.setName = function(fullName) {
+  console.log(this);
   const names = fullName.split(" ");
   console.log(names);
   this.firstName = names[0];
@@ -22,17 +37,26 @@ Person.prototype.setName = function(fullName) {
   console.log(this.lastName);
 };
 
-// prototype proerty - prototype is an object
+// prototype property - prototype is an object
 // on this object we put everything we want to share with the instances of this constructor function i.e. if want all instances to access some sort of method, it get provided here
 // usually it is methods (functions) that are put on the prototype object (although it can be any value you like - not that there is much purpose to that)
 // =================================================
 
-const me = new Person("Charlotte", "Ellwood", 35); // constructor function - functions that get used with the 'new' operator (in this case the constructor function is 'Person)
+const me = new Person("Charlotte", "Ellwood", 35, ["karate", "biking"]); // constructor function - functions that get used with the 'new' operator (in this case the constructor function is 'Person)
+
+// me.getBio = function() {
+//   return "This is fake!";
+// };
+// setting the getBio instance on the 'me' object instance does NOT change the getBio instance on otherPerson
+// js will first check on the 'me' object for getBio, if it finds it it will not go further up the prototype chain to find any other getBio properties
+
 me.setName("Alexis Turner");
 console.log(me.getBio());
-const otherPerson = new Person("Thomas", "Ellwood", 8);
 
-console.log(me.getBio());
+const otherPerson = new Person("Thomas", "Ellwood", 8);
+Person.prototype.getBio = function() {
+  return "Testing Testing";
+};
 console.log(otherPerson.getBio());
 
 // =======================================================================
