09310689af9dd1e9b5a0c319cdd03c0f3f9ea603
char502 <char502@hotmail.com>
2019-02-10 22:56:49 +0000

    vanilla JS: getters and setters review

--- OOP/class_review.js
+++ OOP/class_review.js
@@ -1,3 +1,6 @@
+// Prototypal Inheritance
+// myPerson --> Person.prototype --> Object.prototype --> null
+
 class Person {
   constructor(firstName, lastName, age, likes = []) {
     this.firstName = firstName;
@@ -24,10 +27,59 @@ class Person {
   }
 }
 
-const me = new Person("Charlotte", "Ellwood", 35, ["karate", "biking"]);
-me.setName("Alexis Turner");
-console.log(me.getBio());
+// const me = new Person("Charlotte", "Ellwood", 35, ["karate", "biking"]);
+// me.setName("Alexis Turner");
+// console.log(me.getBio());
 
-const otherPerson = new Person("Thomas", "Ellwood", 8, ["karate", "avatar"]);
+// const otherPerson = new Person("Thomas", "Ellwood", 8, ["karate", "avatar"]);
 
-console.log(otherPerson.getBio());
+// console.log(otherPerson.getBio());
+
+// === Sub Class - a class that inherits behaviour from person ===
+
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes); // gives acces to the parent's constructor function
+    this.position = position;
+  }
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${this.position}`;
+  }
+  getYearsLeft() {
+    return 65 - this.age;
+  }
+}
+
+// const me = new Employee("Charlotte", "Ellwood", 35, "Teacher", [
+//   "karate",
+//   "biking"
+// ]);
+// me.setName("Alexis Turner");
+// console.log(me.getBio());
+// console.log(me.getYearsLeft());
+
+class Student extends Person {
+  constructor(firstName, lastName, age, grade, likes) {
+    super(firstName, lastName, age, likes);
+    this.grade = grade;
+  }
+  getBio() {
+    const status = this.grade >= 70 ? "passing" : "failing";
+
+    return `${this.firstName} is ${status} the class`;
+  }
+  updateGrade(updateGradeAmount) {
+    return `${
+      this.firstName
+    }'s new Grade is: ${(this.grade += updateGradeAmount)}`;
+  }
+}
+
+const firstStudent = new Student("Sophie", "Ellwood", 5, 92, ["gymnastics"]);
+
+console.log(firstStudent);
+console.log(firstStudent.getBio());
+console.log(firstStudent.updateGrade(-30));
+console.log(firstStudent.getBio());
+console.log(firstStudent.updateGrade(35));
+console.log(firstStudent.getBio());
---
+++ OOP/get-set.js
@@ -0,0 +1,7 @@
+const data = {
+  location: ""
+};
+
+// code that uses the data object
+data.location = "Reading";
+console.log(data.location);
